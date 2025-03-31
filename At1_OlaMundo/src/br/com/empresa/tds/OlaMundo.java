936a4095e8ddd3fd0be69c0bb36d662e598a5629
Geovana Ribeiro D Silva <125618145+GEOVANAAPROGRAMMER@users.noreply.github.com>
2025-03-30 22:55:53 -0300

    Delete At1_OlaMundo/src/br/com/empresa/tds directory

--- At1_OlaMundo/src/br/com/empresa/tds/OlaMundo.java
+++
@@ -1,9 +0,0 @@
-package br.com.empresa.tds;
-
-public class OlaMundo {
-	
-	public static void main(String[] args) {
-        System.out.println("Olá, gege!");
-        System.out.println("Meu primeiro programa!");
-    }
-}
